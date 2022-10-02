67360f6cd02291795dd1a7b1b4404975144f5442
Diggory Laycock <nospam@diggory.net>
2022-10-02 12:23:10 +0100

    Initial Commit - a basic sample app to send User Notifications on macOS

--- macOSLocalNotifications/AppDelegate.swift
+++ macOSLocalNotifications/AppDelegate.swift
@@ -6,15 +6,16 @@
 //
 
 import Cocoa
+import UserNotifications
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-	
-
 
 	func applicationDidFinishLaunching(_ aNotification: Notification) {
 		// Insert code here to initialize your application
+		print("app finished launching")
+		UNUserNotificationCenter.current().delegate = self
 	}
 
 	func applicationWillTerminate(_ aNotification: Notification) {
@@ -28,3 +29,11 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
 }
 
+
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+	//	Allow notifications to be presesnted when app is foremost.
+	func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+		return completionHandler([.list, .sound])
+	}
+}
--- macOSLocalNotifications/ViewController.swift
+++ macOSLocalNotifications/ViewController.swift
@@ -6,12 +6,15 @@
 //
 
 import Cocoa
+import Foundation
+import UserNotifications
 
 class ViewController: NSViewController {
+	
+	let userNotificationCentre = UNUserNotificationCenter.current()
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-
 		// Do any additional setup after loading the view.
 	}
 
@@ -21,6 +24,54 @@ class ViewController: NSViewController {
 		}
 	}
 
+	@IBAction func sendNotification(_ sender: Any) {
+		print("sending notification")
+		
+		//	Request for the right to send notifications
+		userNotificationCentre.requestAuthorization(options: [.alert, .sound]) { authorised, error in
+			if authorised {
+				print("App is now authorised to send notifications")
+			} else if !authorised {
+				print("App is NOT authorised to send notifications!")
+			} else {
+				print(error?.localizedDescription as Any)
+			}
+		}
+		
+		userNotificationCentre.getNotificationSettings { settings in
+			if settings.authorizationStatus != .authorized {
+				print("can't send notification, not authorised")
+				return
+			}
+		}
+		
+		let noteContent = UNMutableNotificationContent()
+		
+		noteContent.title = "Notified!"
+		noteContent.subtitle = "We sent a notification"
+		noteContent.body = "You have successfully sent a User Notification"
+		//		noteContent.sound = .default
 
+		//	Custom sound.
+		//	DOES NOT WORK - Always uses the default (Tritone).  Grrr...
+		let blipCafSubfolderNotificationSound = UNNotificationSound(named: UNNotificationSoundName(rawValue: "notificationSoundBlip.caf"))
+		noteContent.sound = blipCafSubfolderNotificationSound
+
+		
+		let id = "test"
+		
+		//	Send just after now…
+		let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 0.1, repeats: false)
+		let request = UNNotificationRequest(identifier: id, content: noteContent, trigger: trigger)
+		
+		//	Actually schedule it
+		userNotificationCentre.add(request) { error in
+			if error != nil {
+				print("Error requesting notification: \(error?.localizedDescription as Any)")
+			}
+		}
+
+	}
+	
 }
 
