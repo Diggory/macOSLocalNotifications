57143c18e4a2c787a2ebc837ceebee2834e98ea1
Diggory Laycock <nospam@diggory.net>
2022-10-05 12:22:13 +0100

    Adds multiple sounds to test.
    Added a variety of different formatted sounds and a UI to pick one.

--- macOSLocalNotifications/ViewController.swift
+++ macOSLocalNotifications/ViewController.swift
@@ -9,14 +9,44 @@ import Cocoa
 import Foundation
 import UserNotifications
 
-class ViewController: NSViewController {
+class ViewController: NSViewController, NSTableViewDelegate, NSTableViewDataSource {
 	
+	var sounds: [String] = []
 	let userNotificationCentre = UNUserNotificationCenter.current()
+	@IBOutlet var filenamesTable: NSTableView!
+
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
+
+		filenamesTable.doubleAction = #selector(tableViewDoubleAction)
+
+		sounds.append("audacity_wav_16bitPCM.wav")
+		sounds.append("audacity.mp3")
+
+		sounds.append("test.caf")
+		sounds.append("test-1.caf")
+		sounds.append("test.m4a")
+		sounds.append("test.wav")
+		
+		sounds.append("dbl08s.wav")
+		sounds.append("ima08s.wav")
+		sounds.append("pcm3208s.wav")
+		sounds.append("sngl08s.wav")
+		sounds.append("ulaw08s.wav")
+
+		sounds.append("M1F1-AlawC-AFsp.aif")
+		sounds.append("M1F1-mulawC-AFsp.aif")
+		
+		sounds.append("notificationSoundBlip_stereo.mp3")
+		sounds.append("notificationSoundBlip.mp3")
+
+		sounds.append("toast.caf")
+		sounds.append("toast.mp3")
+
 	}
+	
 
 	override var representedObject: Any? {
 		didSet {
@@ -24,7 +54,7 @@ class ViewController: NSViewController {
 		}
 	}
 
-	@IBAction func sendNotification(_ sender: Any) {
+	fileprivate func sendNotification(withSound sound: String) {
 		print("sending notification")
 		
 		//	Request for the right to send notifications
@@ -49,14 +79,22 @@ class ViewController: NSViewController {
 		
 		noteContent.title = "Notified!"
 		noteContent.subtitle = "We sent a notification"
-		noteContent.body = "You have successfully sent a User Notification"
+		noteContent.body = "You have successfully sent a User Notification with soundFile: \(sound)"
 		//		noteContent.sound = .default
-
+		
+		//	Check that sound file actually exists
+		let filePath = Bundle.main.bundlePath + "/contents/resources/" + sound
+		print(filePath)
+		let exists = FileManager.default.fileExists(atPath: filePath, isDirectory: nil)
+		guard exists else {
+			print("Sound file not found at: \(filePath)")
+			return
+		}
+		
 		//	Custom sound.
 		//	DOES NOT WORK - Always uses the default (Tritone).  Grrr...
-		let blipCafSubfolderNotificationSound = UNNotificationSound(named: UNNotificationSoundName(rawValue: "notificationSoundBlip.caf"))
+		let blipCafSubfolderNotificationSound = UNNotificationSound(named: UNNotificationSoundName(rawValue: sound))
 		noteContent.sound = blipCafSubfolderNotificationSound
-
 		
 		let id = "test"
 		
@@ -70,7 +108,33 @@ class ViewController: NSViewController {
 				print("Error requesting notification: \(error?.localizedDescription as Any)")
 			}
 		}
+	}
+	
+	// MARK: IB Actions
 
+	@IBAction func sendNotification(_ sender: Any) {
+//		sendNotification(withSound: "toast.mp3")
+		
+		guard filenamesTable.selectedRow != -1 else {
+			print("cannot send notification. No Sound selected")
+			return
+		}
+		sendNotification(withSound: sounds[filenamesTable.selectedRow])
+	}
+
+	@objc func tableViewDoubleAction(sender: AnyObject) {
+//		print("row double clicked")
+		sendNotification(self)
+	}
+	
+	// MARK: TableView protocols
+
+	func numberOfRows(in tableView: NSTableView) -> Int {
+		return sounds.count
+	}
+	
+	func tableView(_ tableView: NSTableView, objectValueFor tableColumn: NSTableColumn?, row: Int) -> Any? {
+		return sounds[row]
 	}
 	
 }
